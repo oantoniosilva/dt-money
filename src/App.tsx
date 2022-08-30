81d8115e525d1ed038d8dc60535aafdc3432e20a
Antonio <antoniopsilva083@gmail.com>
2022-08-29 23:42:05 -0300

    feat: create theme and setup global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <h1>Hi Dev!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Development!</h1>
+    </ThemeProvider>
   )
 }
