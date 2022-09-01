13c9e4285301afa77499085f477b3670d7348eec
Antonio <antoniopsilva083@gmail.com>
2022-09-01 01:08:19 -0300

    feat: create searchform component and styled

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
@@ -9,6 +10,7 @@ export function Transaction() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
